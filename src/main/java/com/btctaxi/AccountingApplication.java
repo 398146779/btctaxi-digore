8abb583717bfa5a7f9da66e5c79bf6c324f9d323
shoulei <shouleizhang@creditease.cn>
2018-08-06 14:32:42 +0800

    123

---
+++ src/main/java/com/btctaxi/AccountingApplication.java
@@ -0,0 +1,45 @@
+package genesis.accounting;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.ErrorMvcAutoConfiguration;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.web.client.RestTemplate;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication(exclude = {ErrorMvcAutoConfiguration.class, DataSourceAutoConfiguration.class}, scanBasePackages = "genesis")
+@EnableAsync
+@EnableAutoConfiguration
+//@EnableEurekaServer
+//@EnableDiscoveryClient
+@EnableSwagger2
+@Configuration
+@MapperScan("genesis.accounting.domain")
+public class AccountingApplication {
+    public static void main(String[] args) {
+        ConfigurableApplicationContext ctx = SpringApplication.run(AccountingApplication.class, args);
+        ctx.registerShutdownHook();
+    }
+
+    @Bean
+    RestTemplate http(RestTemplateBuilder builder) {
+        return builder.build();
+    }
+
+
+//    @Bean
+//    public Docket api() {
+//        return new Docket(DocumentationType.SWAGGER_2)
+//                .select()
+//                .apis(RequestHandlerSelectors.basePackage("com.example.controller"))
+//                .paths(PathSelectors.ant("/foos/*"))
+//                .build() ;
+//    }
+}
