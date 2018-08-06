2d1c7255e52b85c51582e2a1fb92ae9cbed431da
shoulei <shouleizhang@creditease.cn>
2018-08-06 14:37:35 +0800

    12345456

---
+++ src/main/java/com/btctaxi/controller/AddressController.java
@@ -0,0 +1,33 @@
+package genesis.accounting.controller;
+
+import genesis.accounting.service.AddressService;
+import genesis.common.DataMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/address")
+public class AddressController {
+    private AddressService addressService;
+
+    public AddressController(AddressService addressService) {
+        this.addressService = addressService;
+    }
+
+    /**
+     * 获取用户充币地址
+     */
+    @RequestMapping("/query")
+    public DataMap query(@RequestParam("user_id") Long userId, @RequestParam("chain_name") String chainName, @RequestParam("currency_name") String currencyName) {
+        return addressService.query(userId, chainName, currencyName);
+    }
+
+    /**
+     * 获取用户ID
+     */
+    @RequestMapping("/userId")
+    public DataMap queryUserId(@RequestParam("chain_name") String chainName, @RequestParam("address") String address) {
+        return addressService.queryUserId(chainName, address);
+    }
+}
